b1220b8ae998e6eeacae737ca151f3ad545527e1
Jryke <rykerson@gmail.com>
2019-08-25 23:59:16 +0700

    050403

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-Array.from(document.getElementsByClassName('selected')).forEach(el => el.classList.add('primary'))
+Array.from(document.getElementsByClassName('selected')).forEach(el => !el.classList.contains('primary') ? el.classList.remove('selected') : null)
