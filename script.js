e4660fed210b5eb0d8e1b8bde6868634c8a09a71
Jryke <rykerson@gmail.com>
2019-08-25 20:58:22 +0700

    050203

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let mainDOM = document.getElementById('main')
 let mainClassesDOM = document.getElementsByClassName('main')
+let mainClassesDOMArr = Array.from(mainClassesDOM)
+
+console.log(mainClassesDOMArr);
