2739486e1ff037d6ca39e9c37e3e5c5384a93b82
Jryke <rykerson@gmail.com>
2019-08-26 17:48:40 +0700

    050101

--- 050101/script.js
+++ 050101/script.js
@@ -1,9 +1,1 @@
-let productsArr = Array.from(document.getElementsByClassName('product'))
-let productHolder = document.getElementById('products')
-
-productsArr.forEach(div => {
-	// copy divs as children of productHolder
-	productHolder.innerHTML += `<div class="product">${div.innerHTML}</div>`
-	// remove hard coded divs
-	div.parentNode.removeChild(div)
-})
+console.log('Now Connected')
