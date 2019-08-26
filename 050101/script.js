4baf1776959ef652559fd5d2767da4911082fa16
Jryke <rykerson@gmail.com>
2019-08-26 17:55:50 +0700

    050202

--- 050101/script.js
+++ 050101/script.js
@@ -2,5 +2,9 @@
 // console.log('Now Connected')
 
 // 050201
-const mainDOM = document.getElementById('main')
-console.log(mainDOM)
+// const mainDOM = document.getElementById('main')
+// console.log(mainDOM)
+
+// 050202
+const mainClassesDOM = document.getElementsByClassName('main')
+console.log(mainClassesDOM)
