a7efb3e06ffb891d751bcff0a7b81e20fe4374d1
Jryke <rykerson@gmail.com>
2019-08-26 13:25:25 +0700

    050304 - using innerHTML (not appendChild)

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-const errorDOMArr = Array.from(document.getElementsByClassName('error'))
-// I know that there must be a better way of identifying the error class in the coresponding field, but this is the only way I can think of using only what we learned in class.  Please let me know if there is a better way, thank you.
+let productsArr = Array.from(document.getElementsByClassName('product'))
+let productHolder = document.getElementById('products')
 
-const checkName = (element) => {
-	if (!(element.value.length > 2)) {
-		errorDOMArr[0].innerHTML = 'At least 3 characters'
-	} else {
-		errorDOMArr[0].innerHTML = ''
-	}
-}
-
-const checkPassword = (element) => {
-	if (!(element.value.length > 8)) {
-		errorDOMArr[1].innerHTML = 'At least 9 characters'
-	} else {
-		errorDOMArr[1].innerHTML = ''
-	}
-}
+productsArr.forEach(div => {
+	// copy divs as children of productHolder
+	productHolder.innerHTML += `<div class="product">${div.innerHTML}</div>`
+	// remove hard coded divs
+	div.parentNode.removeChild(div)
+})
