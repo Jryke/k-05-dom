f907cbb8d8cf18e0f0125dc72c8421b4534d6ae3
Jryke <rykerson@gmail.com>
2019-08-26 12:49:35 +0700

    050504

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const inputDOM = document.getElementById('email')
-const messageDOM = document.getElementById('message')
+const errorDOMArr = Array.from(document.getElementsByClassName('error'))
+// I know that there must be a better way of identifying the error class in the coresponding field, but this is the only way I can think of using only what we learned in class.  Please let me know if there is a better way, thank you.
 
-const checkEmail = (event) => {
-	event.preventDefault()
-	console.log(inputDOM.value)
-	if (inputDOM.value.includes('@')) {
-		message.innerHTML = 'Thank you for signing up'
-		message.classList.remove('error')
-		message.classList.add('success')
+const checkName = (element) => {
+	if (!(element.value.length > 2)) {
+		errorDOMArr[0].innerHTML = 'At least 3 characters'
 	} else {
-		message.innerHTML = 'Sorry, please provide a valid email address'
-		message.classList.add('error')
+		errorDOMArr[0].innerHTML = ''
+	}
+}
+
+const checkPassword = (element) => {
+	if (!(element.value.length > 8)) {
+		errorDOMArr[1].innerHTML = 'At least 9 characters'
+	} else {
+		errorDOMArr[1].innerHTML = ''
 	}
 }
